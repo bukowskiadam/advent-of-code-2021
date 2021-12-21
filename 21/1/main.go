79766ba5c452b83518b33af4603f235faf5b3e4e
Adam Bukowski <bukowski.adam@gmail.com>
2021-12-21 06:55:09 +0100

    feat: day 21

---
+++ 21/1/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"advent/utils"
+	"fmt"
+)
+
+func main() {
+	lines, _ := utils.ReadLines(utils.ReadFileFromArgs())
+
+	var pos [2]int
+	var points [2]int
+	fmt.Sscanf(lines[0], "Player 1 starting position: %d", &pos[0])
+	fmt.Sscanf(lines[1], "Player 2 starting position: %d", &pos[1])
+
+	fmt.Println(pos)
+
+	pos[0]--
+	pos[1]--
+
+	dice := 1
+	rolls := 0
+	player := 0
+
+	for points[0] < 1000 && points[1] < 1000 {
+		sum := 3*dice + 3
+		pos[player] += sum
+		pos[player] = pos[player] % 10
+		points[player] += pos[player] + 1
+		dice += 3
+		if dice > 100 {
+			dice -= 100
+		}
+		rolls += 3
+		player = (player + 1) % 2
+		fmt.Println(points)
+	}
+
+	loser := 1
+	if points[0] < points[1] {
+		loser = 0
+	}
+	fmt.Println(points[loser] * rolls)
+}
---
+++ 21/2/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"advent/utils"
+	"fmt"
+)
+
+const END_SCORE = 21
+
+type Roll struct {
+	sum   int
+	times int
+}
+
+var possibleRolls []Roll
+
+func play(scores, pos [2]int, player int) (int, int) {
+	if scores[0] >= END_SCORE {
+		return 1, 0
+	}
+	if scores[1] >= END_SCORE {
+		return 0, 1
+	}
+
+	otherPlayer := (player + 1) % 2
+	sum1, sum2 := 0, 0
+	for _, roll := range possibleRolls {
+		var newPos [2]int
+		var newScores [2]int
+		newPos[player] = (pos[player] + roll.sum) % 10
+		newPos[otherPlayer] = pos[otherPlayer]
+		newScores[player] = scores[player] + newPos[player] + 1
+		newScores[otherPlayer] = scores[otherPlayer]
+
+		win1, win2 := play(newScores, newPos, otherPlayer)
+		sum1 += win1 * roll.times
+		sum2 += win2 * roll.times
+	}
+	return sum1, sum2
+}
+
+func main() {
+	lines, _ := utils.ReadLines(utils.ReadFileFromArgs())
+
+	possibleRolls = []Roll{{3, 1}, {4, 3}, {5, 6}, {6, 7}, {7, 6}, {8, 3}, {9, 1}}
+
+	var pos [2]int
+	var scores [2]int
+	fmt.Sscanf(lines[0], "Player 1 starting position: %d", &pos[0])
+	fmt.Sscanf(lines[1], "Player 2 starting position: %d", &pos[1])
+
+	// make it 0-9, instead of 1-10
+	pos[0]--
+	pos[1]--
+
+	player := 0
+
+	w1, w2 := play(scores, pos, player)
+
+	if w1 > w2 {
+		fmt.Println(w1)
+	} else {
+		fmt.Println(w2)
+	}
+}
